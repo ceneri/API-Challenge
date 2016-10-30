24901bb89f9a0dfafc87c4b11b590e978df61a98
ceneri <ceneri@ucsc.edu>
2016-10-30 13:38:03 -0700

    Second step of the challenge, use PHP and JSON to get a string and return its Inverse

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!--
 Cesar Neri
 ceneri@ucsc.edu
+October 30, 2016
+index.php
 -->
 
 
@@ -13,40 +15,15 @@ ceneri@ucsc.edu
     </head>
     
     <header>
-        <b>2040 Challenge</b>
+        <h1>2040 Challenge: Second Part</h1>
     </header>
     
     <body>
+ 
+        <?php require('reverse.php')?> 
         
         <p></p>
         
-        <?php 
-        
-        //Url to send the JSOn to
-        $url = 'http://challenge.code2040.org/api/register';
-                   
-        //Data to be contained inside the JSON
-        $data = array(
-            'token'  => 'f5b79e8901de60674023933c1a5ad4fd',
-            'github' => 'https://github.com/ceneri/API-Challenge.git',
-        );
-
-        //Create JSOn and specify type and method
-        $options = array(
-            'http' => array(
-            'method'  => 'POST',
-            'content' => json_encode( $data ),
-            'header'=>  "Content-Type: application/json\r\n" .
-                "Accept: application/json\r\n"
-            )
-         );  
-        
-        //connection
-         $context  = stream_context_create( $options );
-         $result = file_get_contents( $url, false, $context );
-         $response = json_decode( $result );
-
-        ?> 
     </body>
     
     <footer>
---
+++ reverse.php
@@ -0,0 +1,77 @@
+
+<!--
+Cesar Neri
+ceneri@ucsc.edu
+October 30, 2016
+reverse.php
+-->
+
+<?php
+
+        //Url to send the JSOn to
+        $url = 'http://challenge.code2040.org/api/reverse';
+        $url2 = 'http://challenge.code2040.org/api/reverse/validate';
+        $reverse = "";
+                   
+        //Data to be contained inside the JSON
+        $data = array(
+            'token'  => 'f5b79e8901de60674023933c1a5ad4fd',
+        );
+
+        //Create JSOn and specify type and method
+        $options = array(
+            'http' => array(
+            'method'  => 'POST',
+            'content' => json_encode( $data ),
+            'header'=>  "Content-Type: application/json\r\n" .
+                "Accept: application/json\r\n"
+            )
+        );  
+        
+        //connection
+        $context  = stream_context_create( $options );
+        //Response is a String
+        $result = file_get_contents( $url, false, $context );
+         
+        //print String response
+        echo ("<p>String response: ");
+        echo $result . "</p>";
+         
+        //Transfrom String to array
+        $string_array = str_split($result);
+         
+        //add all characters from response to string in reverse order
+        for ($i = count($string_array) - 1; $i >= 0; $i--){
+            $reverse .= $string_array[$i];
+            //$reverse = $string_array[$i];
+        }
+         
+        //print String response
+        echo ("<p>String reversed: ");
+        echo $reverse . "</p>";
+        
+        //Data to be contained inside the response JSON
+        $data2 = array(
+            'token'  => 'f5b79e8901de60674023933c1a5ad4fd',
+            'string' => $reverse,
+        );
+        
+        //Create response JSOn and specify type and method
+        $options2 = array(
+            'http' => array(
+            'method'  => 'POST',
+            'content' => json_encode( $data2 ),
+            'header'=>  "Content-Type: application/json\r\n" .
+                "Accept: application/json\r\n"
+            )
+        );
+        
+        //connection
+        $context2  = stream_context_create( $options2 );
+        //Response is a String
+        $result2 = file_get_contents( $url2, false, $context2 );
+         
+        //Response in JSOn
+        //$response = json_decode( $result );
+
+?>
